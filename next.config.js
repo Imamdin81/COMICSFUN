4a44bd8667119f494e0645b8eee59b7e69a38b02
Imam Din <41516850+Imamdin81@users.noreply.github.com>
2023-09-23 17:22:19 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,23 @@
 module.exports = {
   reactStrictMode: true,
-  redirects: async () => {
+  async redirects() {
     return [
+      // Redirect from one domain to another
       {
         source: '/about',
         destination: '/',
         permanent: false,
       },
+      // Redirect from one domain's path to another
       {
         source: '/old-blog/:id',
         destination: '/new-blog/:id',
+        permanent: true,
+      },
+      // Redirect from one domain to a different domain
+      {
+        source: '/example-page',
+        destination: 'https://example.com/new-page',
         permanent: true,
       },
     ];
